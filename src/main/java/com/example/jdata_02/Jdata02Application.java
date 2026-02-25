f9bc2959290cc3082101894eb398a231461a997b
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-02-25 17:55:31 +0300

    Initial commit

---
+++ src/main/java/com/example/jdata_02/Jdata02Application.java
@@ -0,0 +1,13 @@
+package com.example.jdata_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdata02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdata02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdata_02/Jdata02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdata_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdata02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
